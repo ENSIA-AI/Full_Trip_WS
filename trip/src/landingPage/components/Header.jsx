ed3c1dd34fff1a864941acbe2cd6f57b81a3890a
kderKiro <abdelkadir.megdoud@ensia.edu.dz>
2025-10-13 11:21:08 +0100

    3

--- trip/src/landingPage/components/Header.jsx
+++ trip/src/landingPage/components/Header.jsx
@@ -2,18 +2,23 @@ import '../styles/header&signUp.css'
 import React from 'react'
 import titIcon from '../images/title-icon.svg'
 import SignUpForm from './SignUpForm'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Header() {
   const [formAppear, setFormAppear] = React.useState(false);
   let [btnContent, setBtnContent] = React.useState('Sign Up');
   const [navBarContent] = React.useState(['Stays', 'Flights', 'Car rental', 'Airport taxis', 'attractions']);
   const navContent = navBarContent.map(navContent => (
-    <li tabindex="0" key={navContent}>{navContent}</li>
+    <li data-aos='zoom-in' tabindex="0" key={navContent}>{navContent}</li>
   ))
 
   function showForm() {
     setFormAppear(true);
   }
+  React.useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
 
   React.useEffect(() => {
     const home = document.querySelector('.main-page');
@@ -40,12 +45,12 @@ export default function Header() {
   return (
     <div className='header-div'>
       <header>
-        <div className="slogo">
+        <div data-aos='fade-right' className="slogo">
           <img src={titIcon}></img>
           <h1>TravelWUs</h1>
         </div>
         <ul className='navBar'>{navContent}</ul>
-        <div className='SignLogIn'>
+        <div data-aos='fade-left' className='SignLogIn'>
           <button onClick={showForm}>{btnContent}</button>
         </div>
       </header>
