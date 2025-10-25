d0e69d86f157dd7ea7edc9f6332efea32d07c423
Oussama x7 <bouzianeoussama279@gmail.com>
2025-10-25 04:04:40 +0100

    25 oct

--- trip/src/pages/Hotels.jsx
+++ trip/src/pages/Hotels.jsx
@@ -14,6 +14,25 @@ import top10 from './pics/top10.jpg'
 
 import './css/page.css'
 function Hotels() {
+
+    const slider = document.querySelector('.slider');
+    let scrollAmount = 0;
+
+    setInterval(() => {
+        const slideWidth = slider.clientWidth;
+        scrollAmount += slideWidth;
+
+        if (scrollAmount >= slider.scrollWidth) {
+            scrollAmount = 0;
+        }
+
+        slider.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    }, 3000);
+
+
     return (<>
 
         <Navbar></Navbar>
@@ -22,16 +41,82 @@ function Hotels() {
             <section className="container">
                 <div className="slider-wrapper">
                     <div className="slider">
+
+                    <div className="slide">
                         <img id="slide-1" src={top1} alt="top1" />
+                        <div className="pic-overlay">
+                            <h2>1. Capella Bangkok (Bangkok, Thailand)</h2>
+                        </div>
+                    </div>
+
+
+                    <div className="slide">
                         <img id="slide-2" src={top2} alt="top2" />
+                        <div className="pic-overlay">
+                            <h2>2. Passalacqua (Lake Como, Italy)</h2>
+                        </div>
+                    </div>
+
+
+                    <div className="slide">
                         <img id="slide-3" src={top3} alt="top3" />
+                        <div className="pic-overlay">
+                            <h2>3. Rosewood Hong Kong (Hong Kong)</h2>
+                        </div>
+                    </div>
+
+
+
+                    <div className="slide">
                         <img id="slide-4" src={top4} alt="top4" />
+                        <div className="pic-overlay">
+                            <h2>4. Cheval Blanc Paris (Paris, France)</h2>
+                        </div>
+                    </div>
+
+
+                    <div className="slide">
                         <img id="slide-5" src={top5} alt="top5" />
+                        <div className="pic-overlay">
+                            <h2>5. The Upper House (Hong Kong)</h2>
+                        </div>
+                    </div>
+
+
+                    <div className="slide">
                         <img id="slide-6" src={top6} alt="top6" />
+                        <div className="pic-overlay">
+                            <h2>6. Raffles Singapore (Singapore)</h2>
+                        </div>
+                    </div>
+
+
+
+                    <div className="slide">
                         <img id="slide-7" src={top7} alt="top7" />
+                        <div className="pic-overlay">
+                            <h2>7. Aman Tokyo (Tokyo, Japan)</h2>
+                        </div>
+                    </div>
+                    <div className="slide">
                         <img id="slide-8" src={top8} alt="top8" />
+                        <div className="pic-overlay">
+                            <h2>8. Soneva Fushi (Maldives)</h2>
+                        </div>
+                    </div>
+
+                    <div className="slide">
                         <img id="slide-9" src={top9} alt="top9" />
+                        <div className="pic-overlay">
+                            <h2>9. Atlantis The Royal (Dubai, UAE)</h2>
+                        </div>
+                    </div>
+
+                    <div className="slide">
                         <img id="slide-10" src={top10} alt="top10" />
+                        <div className="pic-overlay">
+                            <h2>10. Nihi Sumba (Sumba Island, Indonesia)</h2>
+                        </div>
                     </div>
                     <div className="slider-nav">
                         <a href="#slide-1"></a>
@@ -45,8 +130,12 @@ function Hotels() {
                         <a href="#slide-9"></a>
                         <a href="#slide-10"></a>
                     </div>
+                    </div>
                 </div>
             </section>
+        </div>
+        <div className="second-section" >
+        <h1>Wanaa reserve ?? </h1>
         </div>
         <div className="output">
             <Searcharea></Searcharea>
