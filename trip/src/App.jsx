76d65077270325e86da6da8e2d5bce0465961747
Oussama x7 <bouzianeoussama279@gmail.com>
2025-11-26 16:33:57 +0100

    Update App.jsx

---
+++ trip/src/App.jsx
@@ -0,0 +1,34 @@
+import Header from './landingPage/components/Header'
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+
+import UserProfile from './UserProfile/UserProfile'
+
+import Register from './landingPage/components/Register'
+
+
+import { useState } from 'react'
+
+
+
+
+
+function App() {
+
+  const [UserInfo,setUserInfo]=useState({UserName:"Default",UserType:"Agency"});
+  
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate to="/Home" replace/>}></Route>
+        <Route path="/*" element={<Header setUserInfo={setUserInfo} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/Profile/*" element={<UserProfile Username={UserInfo.UserName} U_type={UserInfo.UserType}/>} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
