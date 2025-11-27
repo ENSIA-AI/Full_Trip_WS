4d5662617e5a4ac3bdaaebfa261442150fa26f95
kderKiro <abdelkadir.megdoud@ensia.edu.dz>
2025-11-27 10:11:07 +0100

    asdf

--- trip/src/landingPage/components/Explore.jsx
+++ trip/src/landingPage/components/Explore.jsx
@@ -36,15 +36,15 @@ export default function Explore() {
       <p data-aos='fade-up'>EXPLORE NOW</p>
       <h1 data-aos='fade-up'>Find Your Dream Destination</h1>
       <small data-aos='fade-up'>Fill in the fields below to find the best spot for your next tour</small>
-      <form className='trip-form' action={onsubmit}>
-        <label data-aos='fade-up'>
-          {location ? (<i class='bxr  bx-location-check'  ></i>) : (<i class='bxr  bx-location'  ></i>)}<input   id='LocInp' name='location' type='text' placeholder='Location'></input>
+      <form className='tr-form' action={onsubmit}>
+        <label className='kra' data-aos='fade-up'>
+          {location ? (<i class='bxr  bx-location-check'  ></i>) : (<i class='bxr  bx-location'  ></i>)}<input   className='kr' id='LocInp' name='location' type='text' placeholder='Location'></input>
         </label>
-        <label data-aos='fade-up'>
-          <i class='bxr  bx-wallet-note'  ></i><input type='number' placeholder='Budget'></input>
+        <label className='kra' data-aos='fade-up'>
+          <i class='bxr  bx-wallet-note'  ></i><input className='kr' type='number' placeholder='Budget'></input>
         </label>
-        <label data-aos='fade-up'>
-          <i class='bxr  bx-calendar-minus'  ></i><input type='date' placeholder='Date'></input>
+        <label className='kra' data-aos='fade-up'>
+          <i class='bxr  bx-calendar-minus'  ></i><input className='kr' type='date' placeholder='Date'></input>
         </label>
         <button data-aos='fade-left'><i class='bxr  bx-search'  ></i> Search</button>
       </form>
